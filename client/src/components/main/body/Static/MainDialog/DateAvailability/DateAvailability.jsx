b0a2d72de8d6ef4815c79f512163283a74a95411
slasmi-makor <slasmi@makor-capital.com>
2024-12-08 16:14:56 +0200

    add logic to fee datea

--- client/src/components/main/body/Static/MainDialog/DateAvailability/DateAvailability.jsx
+++ client/src/components/main/body/Static/MainDialog/DateAvailability/DateAvailability.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import DateAvailabilityView from "./DateAvailability.view";
 import ApiRooms from "../../../../../../apis/roomsRequest";
 import { useSelector } from "react-redux";
+import { TrendingUp } from "@mui/icons-material";
 
 const DateAvailability = () => {
   const vacationId = useSelector((state) => state.vacationSlice.vacationId);
   const form = useSelector((state) => state.staticSlice.form);
+  const [gaps, setGaps] = useState([])
+  const [loading, setloading] = useState(false)
   const token = sessionStorage.getItem("token");
   const vacationsDates = useSelector(
     (state) => state.vacationSlice.vacationsDates
@@ -18,23 +21,29 @@ const DateAvailability = () => {
       if (endDate === "") {
         endDate = vacationsDates[vacationsDates.length - 2].end_date; // If empty, use the previous object's end date
       }
-      console.log(form)
-      const response = ApiRooms.getUnAndAvailableDates(
+      
+      const response = await ApiRooms.getUnAndAvailableDates(
         token,
         form.rooms_id,
         vacationId,
         startDate,
         endDate
       );
+      console.log(response)
+      if(response.data.length > 0){
+        setloading(TrendingUp)
+      }
+      setGaps(response.data)
+      console.log(response)
     } catch (error) {
       console.log(error);
     }
   };
   useEffect(() => {
-    getUnAndAvailableDates();
+   getUnAndAvailableDates();
   }, []);
 
-  return <DateAvailabilityView />;
+  return loading ? <DateAvailabilityView gaps={gaps}/> :<></>;
 };
 
 export default DateAvailability;
